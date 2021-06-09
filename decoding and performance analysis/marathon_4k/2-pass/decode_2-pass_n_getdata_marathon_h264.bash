a4d7b74f5168acad1189a1e73a164860133a3cdd
federicoserraino <85652674+federicoserraino@users.noreply.github.com>
2021-06-10 01:45:33 +0200

    Add files via upload

---
+++ decoding and performance analysis/marathon_4k/2-pass/decode_2-pass_n_getdata_marathon_h264.bash
@@ -0,0 +1,49 @@
+#!/bin/bash
+# MARATHON - H264 - TWO PASS DECODING (with ffmpeg) AND GET DATA
+
+codec="h264"
+encoder_name="AVC"
+org_seq_name="marathon_4k.yuv"
+seq_name="marathon_2-pass"
+w=3840
+h=2160
+n=150
+fps=30
+
+curr_dir=`pwd`
+dest_dir="${codec}_${seq_name}"
+output_file="marathon_2-pass_4k.csv"
+vmaf_dir="/home/ubuntu/vmaf" # Set param with your vmaf path
+
+### Necessary to compute VMAF value
+cd ${vmaf_dir} && source .venv/bin/activate && cd ${curr_dir}
+
+decode_n_getdata() {
+# DECODING
+ffmpeg -y -i ${dest_dir}/${seq_name}_${i}.${codec} -vsync 0 -vcodec rawvideo -f rawvideo ${dest_dir}/${seq_name}_${i}.yuv
+# GET BITRATE
+bitrate=`grep 'kb/s' ${dest_dir}/encdata_${i}.txt | tail -1 | awk -F ':' '{print $2}'`
+# GET ENCODING TIME
+enctime=`./sum_n_get_2-pass_enctime ${dest_dir}/enctime_1st-pass_${i}.txt ${dest_dir}/enctime_2nd-pass_${i}.txt | grep 'User+System_total_time' | awk -F ':' '{print $2}'`
+# GET PSNR
+psnr=`./psnr_video ${org_seq_name} ${dest_dir}/${seq_name}_${i}.yuv ${w} ${h} ${n}`
+# GET VMAF
+cd ${vmaf_dir}
+
+(PYTHONPATH=python ${vmaf_dir}/python/vmaf/script/run_vmaf.py \
+yuv420p ${w} ${h} \
+${curr_dir}/${org_seq_name} \
+${curr_dir}/${dest_dir}/${seq_name}_${i}.yuv \
+--model model/vmaf_4k_v0.6.1.pkl --out-fmt json) &> ${curr_dir}/vmaf.json
+
+cd ${curr_dir}
+vmaf=`tail -4 vmaf.json | grep 'score' | awk -F ': ' '{print $2}' | sed -r 's/.{13}$//'`
+# WRITE DATA
+echo "${encoder_name};${bitrate};${psnr};${vmaf};${enctime}" >> ${output_file}
+}
+
+for i in {5000..30000..2500}
+do
+    decode_n_getdata
+done
+
---
+++ decoding and performance analysis/marathon_4k/2-pass/decode_2-pass_n_getdata_marathon_svtav1.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+# MARATHON - SVT_AV1 - TWO PASS DECODING AND GET DATA
+
+codec="av1"
+encoder_name="SVTAV1"
+org_seq_name="marathon_4k.yuv"
+seq_name="marathon_2-pass"
+w=3840
+h=2160
+n=150
+fps=30
+
+curr_dir=`pwd`
+dest_dir="${encoder_name}_${seq_name}"
+output_file="marathon_2-pass_4k.csv"
+vmaf_dir="/home/ubuntu/vmaf" # Set param with your vmaf path
+
+### Necessary to compute VMAF value
+cd ${vmaf_dir} && source .venv/bin/activate && cd ${curr_dir}
+
+decode_n_getdata() {
+# DECODING
+ffmpeg -y -i ${dest_dir}/${seq_name}_${i}.${codec} -vcodec rawvideo -f rawvideo ${dest_dir}/${seq_name}_${i}.yuv
+# GET BITRATE
+bitrate=`grep 'kbps' ${dest_dir}/encdata_${i}.txt | tail -1 | awk '{print $5}'`
+# GET ENCODING TIME
+enctime=`./sum_n_get_2-pass_enctime ${dest_dir}/enctime_1st-pass_${i}.txt ${dest_dir}/enctime_2nd-pass_${i}.txt | grep 'User+System_total_time' | awk -F ':' '{print $2}'`
+# GET PSNR
+psnr=`./psnr_video ${org_seq_name} ${dest_dir}/${seq_name}_${i}.yuv ${w} ${h} ${n}`
+# GET VMAF
+cd ${vmaf_dir}
+
+(PYTHONPATH=python ${vmaf_dir}/python/vmaf/script/run_vmaf.py \
+yuv420p ${w} ${h} \
+${curr_dir}/${org_seq_name} \
+${curr_dir}/${dest_dir}/${seq_name}_${i}.yuv \
+--model model/vmaf_4k_v0.6.1.pkl --out-fmt json) &> ${curr_dir}/vmaf.json
+
+cd ${curr_dir}
+vmaf=`tail -4 vmaf.json | grep 'score' | awk -F ': ' '{print $2}' | sed -r 's/.{13}$//'`
+# WRITE DATA
+echo "${encoder_name};${bitrate};${psnr};${vmaf};${enctime}" >> ${output_file}
+}
+
+for i in {5000..30000..2500}
+do
+    decode_n_getdata
+done
